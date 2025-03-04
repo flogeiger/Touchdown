65b2fbd1810b4b6cc32491c348c2221c1a87d7cf
fgeiger <florian.geiger21@gmail.com>
2025-03-04 14:07:11 +0100

    added all Icons, Images, created Footer and created Starup Image

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Florian Geiger on 04.03.25.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
