64e9db542a475528e98f4cd4000c5234641bf643
fgeiger <florian.geiger21@gmail.com>
2025-03-04 13:48:47 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Florian Geiger on 04.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
