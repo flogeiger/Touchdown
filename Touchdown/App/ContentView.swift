f261605a73d4fa946b2af23648e527aefa7f6e1e
fgeiger <florian.geiger21@gmail.com>
2025-03-04 15:10:47 +0100

    created custom navigation Bar

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -13,7 +13,21 @@ struct ContentView: View {
     //MARK: - BODY
     
     var body: some View {
-        FooterView().padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal,15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 0)
+                
+                Spacer()
+                
+                
+                FooterView().padding(.horizontal)
+            }.background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }.ignoresSafeArea(.all, edges: .top)
     }
 }
 
